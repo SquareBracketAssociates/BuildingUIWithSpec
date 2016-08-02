c78a74d5cb50b44e80a95934b8a36127fe842deb
Yann Dubois <yann59.dubois@gmail.com>
2016-08-02 14:17:58 +0200

    Update copySupport for MacOS

--- copySupport.mk
+++ copySupport.mk
@@ -1,4 +1,4 @@
 copySupport: initDir
-	find . -type d -path $(OUTPUTDIRECTORY) -prune -o -wholename "*/figures" -exec cp {} --parents -r ${OUTPUTDIRECTORY} \;
-	cp -r support/ $(OUTPUTDIRECTORY)
+	find . -type d -path $(OUTPUTDIRECTORY)/ -prune -o -wholename "*/figures" -exec rsync -rR {} ${OUTPUTDIRECTORY} \;
+	cp -R support $(OUTPUTDIRECTORY)
 	cp .latexmkrc $(OUTPUTDIRECTORY)
